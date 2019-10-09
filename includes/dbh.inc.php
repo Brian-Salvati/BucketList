6c9ca5ac50aaabbb179c8e2c27eff4145fc8ed2e
Brian Salvati <briancsalvati@gmail.com>
2019-10-09 17:44:50 -0400

    Created error handlers for login system

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,1 +1,13 @@
 <?php
+
+$servername = "localhost";
+$dbUsername = "root";
+$dbPassword = "xj0461";
+$dbName = "bucket_db";
+
+$conn = mysqli_connect($servername, $dbUsername, $dbPassword, $dbName);
+
+if (!$conn) {
+    die("Connection failed: ".mysqli_connect_error());
+}
+
--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -1,5 +1,83 @@
 <?php
 // Checks if user got to signup page legitimately
-if (isset($_post['signup-submit'])) {
+if (isset($_POST['signup-submit'])) {
 
+    require 'dbh.inc.php';
+
+    // Saves fields from user signup form
+    $username = $_POST['uid'];
+    $email = $_POST['mail'];
+    $password = $_POST['pwd'];
+    $passwordRepeat = $_POST['pwd-repeat'];
+
+    // Checks if any fields are empty
+    if (empty($username)  || empty($email)  || empty($password)  || empty($passwordRepeat)) {
+        // Keeps username and email fields filled out when user is sent back to signup page
+        header("Location: ../signup.php?error=emptyfields&uid=".$username."&mail=".$email);
+        // Stops script from running if user makes mistake
+        exit();
+    }
+    // Checks if both email and username are invalid
+    else if (!filter_var($email, FILTER_VALIDATE_EMAIL) && !preg_match("/^[a-zA-z0-9]*$/", $username)) {
+        // If invalid email and username, no fields will be sent back to signup page
+        header("Location: ../signup.php?error=invalidmailuid");
+        exit();
+    }
+    // Checks if invalid email using filter
+    else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        // If invalid email, only username will be sent back to signup page
+        header("Location: ../signup.php?error=invalidmail&uid=".$username);
+        exit();
+    }
+    // Checks if invalid username using pattern
+    else if (!preg_match("/^[a-zA-z0-9]*$/", $username)) {
+        // If invalid username, only email will be sent back to signup page
+        header("Location: ../signup.php?error=invaliduid&mail=".$email);
+        exit();
+    }
+    // Checks if passwords match
+    else if ($password !== $passwordRepeat) {
+        header("Location: ../signup.php?error=passwordcheckuid=".$username."&mail=".$email);
+        exit();
+    }
+    else {
+        $sql = "SELECT username FROM users WHERE username=?";
+        // Prepared statement 
+        $stmt = mysqli_stmt_init($conn);
+        if (!mysqli_stmt_prepare($stmt, $sql)) {
+            header("Location: ../signup.php?error=sqlerror");
+            exit();
+        }
+        else {
+            // Binds params from user to sql statement
+            mysqli_stmt_bind_param($stmt, "s", $username);
+            // Runs query on db
+            mysqli_stmt_execute($stmt);
+            // Stores result in $stmt
+            mysqli_stmt_store_result($stmt);
+            // $resultCheck is equal to the number of rows returned from db. Should be 0 or 1 in this case
+            $resultCheck = mysqli_stmt_num_rows($stmt);
+            // If $resultCheck > 0 then the username already exists in db
+            if ($resultCheck > 0) {
+                header("Location: ../signup.php?error=usertaken&mail=".$email);
+                exit();
+            }
+            else {
+                $sql = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
+                $stmt = mysqli_stmt_init($conn);
+                if (!mysqli_stmt_prepare($stmt, $sql)) {
+                    header("Location: ../signup.php?error=sqlerror");
+                    exit();
+                }
+                else {
+                    // Binds params from user to sql statement
+                    mysqli_stmt_bind_param($stmt, "sss", $username, $email, $password);
+                    // Runs query on db
+                    mysqli_stmt_execute($stmt);
+                    // Stores result in $stmt
+                    mysqli_stmt_store_result($stmt);
+                }
+            }
+        }
+    }
 }
